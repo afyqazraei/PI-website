1fb5136ad408ba3a86bd422a11c00dddf03468c3
Giulia Fazzino <gfazzino@chameleon.physi.uni-heidelberg.de>
2025-02-11 19:38:14 +0100

    Boxes can scroll

--- buttons.js
+++ buttons.js
@@ -1,8 +1,18 @@
 // Open the corresponding dropdown menu when the button is clicked
 document.querySelectorAll('.dropdown-btn').forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function(event) {
         let dropdownContent = this.nextElementSibling;
-        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+        
+        // Get dynamic coordinates of the dropdown button to place the dropdown menu
+        const rect = button.getBoundingClientRect();
+        document.documentElement.style.setProperty('--dropdown-left', `${rect.left}px`); 
+        
+        // Toggle visibility of the dropdown
+        if (dropdownContent.style.display === 'block') {
+            dropdownContent.style.display = 'none';
+        } else {
+            dropdownContent.style.display = 'block';
+        }
     });
 });
 
--- main_page.php
+++ main_page.php
@@ -17,28 +17,25 @@ include("header.php");
 <body lang="en-US" dir="ltr">
 <br/>
 	<h1 class="western">Welcome to the High Energy  Group</h1> 
-	<p style="margin-bottom:   0cm"> 
 
 	<!-- Collage of our pictures (TODO: can we write a loop?)-->
 	<div class="row" style="text-align: center;">
-		<a href="https://en.wikipedia.org/wiki/Cat">
-			<img src="./figures/test_images/cat.99.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.997.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.984.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.985.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.99.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.997.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.984.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="figures/test_images/cat.985.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="figures/test_images/cat.987.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="figures/test_images/cat.988.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="figures/test_images/cat.989.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="figures/test_images/cat.990.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.99.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.997.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="./figures/test_images/cat.984.jpg" width="100" height="100" style="object-fit: cover;">
-			<img src="figures/test_images/cat.985.jpg" width="100" height="100" style="object-fit: cover;">
-		</a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.99.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.997.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.984.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.985.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.99.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.997.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.984.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="figures/test_images/cat.985.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="figures/test_images/cat.987.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="figures/test_images/cat.988.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="figures/test_images/cat.989.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="figures/test_images/cat.990.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.99.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.997.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="./figures/test_images/cat.984.jpg" width="100" height="100" style="object-fit: cover;"></a>
+		<a href="https://en.wikipedia.org/wiki/Cat"><img src="figures/test_images/cat.985.jpg" width="100" height="100" style="object-fit: cover;"></a>
 	</div>
 		
 	<p style="margin-bottom: 0cm">
@@ -46,36 +43,42 @@ include("header.php");
                 with a focus on the study of fundamental particles and their interactions.
                 This includes the participation in the ATLAS experiment at the Large Hadron Collider (LHC) at CERN,
                 the Mu3e experiment at PSI and the development of new high resolution Silicon detector technologies.
-	
-	
-	<h2>Our Research Subgroups</h2>
-
-	<div class ="dropdown-container">
-	<div class="dropdown">
-    <button class="dropdown-btn">Mu3e</button>
-    	<div class="dropdown-content">
-		<a href="#">Hi this is mu3e</a>
-    	</div>
-  	</div>
-	<div class="dropdown">
-    <button class="dropdown-btn">ATLAS</button>
-    	<div class="dropdown-content">
-		<a href="#">Hi I'm ATLAS</a>
-    	</div>
-  	</div>
-	<div class="dropdown">
-    <button class="dropdown-btn">HV-MAPS</button>
-    	<div class="dropdown-content">
-		<a href="#">Hi I'm HV-MAPS</a>
-    	</div>
-	</div>
-	</div>
 	</p>
-	<p style="margin-bottom: 0cm"><a href="#top"></a></p>
+	
+	<div class="content">
+  <h2>Our Research Subgroups</h2>
 
+  <div class="dropdown-container">
+
+    <div class="dropdown">
+      <button class="dropdown-btn">Mu3e</button>
+      <div class="dropdown-content">
+        <a href="#">Hi this is mu3e</a>
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button class="dropdown-btn">ATLAS</button>
+      <div class="dropdown-content">
+        <a href="#">Hi I'm ATLAS</a>
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <button class="dropdown-btn">HV-MAPS</button>
+      <div class="dropdown-content">
+        <a href="#">Hi I'm HV-MAPS</a>
+      </div>
+    </div>
+
+  </div>
+<div id="dropdown-content-placeholder"></div>
 
 </div>
+	<p> Hi I am some content</p>
 
+
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
 <!-- JavaScripts -->
 <script type="text/javascript" src="buttons.js"></script>
