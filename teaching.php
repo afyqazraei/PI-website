4f23944229673a344d744ae657a9b8612cb57843
fritz <fritz@physi.uni-heidelberg.de>
2025-02-18 22:08:01 +0100

    teaching is now copied from Prof. Schönings own Website

--- teaching.php
+++ teaching.php
@@ -5,14 +5,13 @@ include($incpath . "/config.php");
 include($headerInc);
 ?>
 
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PI Main webpage</title>
-	<!-- Include style file -->
+    <!-- Include style file -->
     <link rel="stylesheet" type="text/css" href="<?php echo $designCss;?>">
 </head>
 
@@ -22,9 +21,30 @@ include($headerInc);
 
 <!-- ++++++++++++++++++++ Start Main Content of the page here! +++++++++++++++++++++ -->
         <h1 class="western">Teaching</h1> 
-        <h2 class="westers">Courses by Prof André Schöning</h2>
-        <iframe src="https://www.physi.uni-heidelberg.de/~schoning/Vorlesungen/" width="1000" height="800"></iframe>
         
+        <h2 class="western">Courses taught by Prof. Dr. Schöning</h2>
+
+        <?php
+        // Fetch the content from an external PHP file
+        $url = "https://www.physi.uni-heidelberg.de/~schoning/Vorlesungen/";
+        $content = file_get_contents($url);
+
+        if ($content !== false) {
+            // Remove everything before the main content
+            $content = preg_replace('/.*?<h1>Prof\. Dr\. Andre Sch&ouml;ning<\/h1>/s', '', $content);
+
+            $content = preg_replace('/.<h2>Vorlesungen und Lehrveranstaltungen<\/h2>/s', '<h3>Lectures and Seminars</h3>', $content);
+            $content = preg_replace('/.<h2>frühere Vorlesungen und Lehrveranstaltungen<\/h2>/s', '<h3>earlier Lectures</h3>', $content);
+
+            // Remove everything after the footer (closing body, scripts, additional HTML)
+            $content = preg_replace('/<\/body>.*<\/html>/s', '</body></html>', $content);
+
+            // Output the cleaned content
+            echo $content;
+        } else {
+            echo "<p>Error fetching content.</p>";
+        }
+        ?>
         <h2 class="western">Courses taught by other members of the group</h2>
         <h2 class="western">Winter Semester 2024 - 2025</h2> 
         <ul> 
@@ -37,7 +57,6 @@ include($headerInc);
             <li>News 2</li>
         </ul>
 <!-- ++++++++++++++++++++ End Main Content of the page here! +++++++++++++++++++++ -->
-
 
     </div>
 </body>
