ebf3eb24adc43d2fe9917440b6e3311ee191f23f
fritz <fritz@physi.uni-heidelberg.de>
2025-02-18 11:46:05 +0100

    created research directory for the subgroup pages

--- people_old.php
+++ people_old.php
@@ -1,6 +1,6 @@
 <?php
-$subpath = dirname($_SERVER['REQUEST_URI']);
-$incpath = "../assets/inc";
+$subpath = $_SERVER['REQUEST_URI'];
+$incpath = "assets/inc";
 include($incpath . "/config.php");
 include($headerInc);
 ?>
@@ -10,16 +10,16 @@ include($headerInc);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HV-MAPS Publications</title>
+    <title>Group-Members</title>
 	<!-- Include style file -->
     <link rel="stylesheet" type="text/css" href="<?php echo $designCss;?>">
 
 </head>
 <body lang="en-US" dir="ltr" style="text-align:left;">
 
-<h1>Lists of Publications, Talks & Posters</h1>
+<h1>Group Members</h1>
 
-<h2>Publications</h2>
+<p>Here you can find a list of people working in my group.</p>
 
 </body>
 </html>
